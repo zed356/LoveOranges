2b6c42367e6fdbb4a188b175755aab9bee9b8938
Z Ta <zilvistam@gmail.com>
2024-09-24 19:22:23 +0100

    added concurrently, 'npm start' launches both frontend and backend

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <h1 className="text-3xl font-bold underline text-red-600">
       Simple React Typescript Tailwind Sample another test
+      <img
+        alt="orange"
+        src="https://www.liveeatlearn.com/wp-content/uploads/2022/11/Lima-Orange.jpg"
+      />
     </h1>
   );
 }
