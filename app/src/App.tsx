9d06a0961c71443080c1050f95b250a473d0e5b2
Z Ta <zilvistam@gmail.com>
2024-09-24 18:54:43 +0100

    test

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
+      Simple React Typescript Tailwind Sample another test
     </h1>
   );
 }
